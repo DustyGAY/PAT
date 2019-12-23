a98907c077825fda186f75e63474de108d0184df
DustyGAY <1286856721@qq.com>
2019-12-23 19:02:09 +0800

    修改代码格式

--- PAT-1001/PAT-1001.cpp
+++ PAT-1001/PAT-1001.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
 int main(){
-	int n_num;
-	std::cin >> n_num;
-	int i;
-	for(i = 0;n_num != 1; i++){
-		if(n_num % 2)
-			n_num = (3 * n_num + 1) / 2;
+	int num;
+	std::cin >> num;
+	int count;
+	for(count = 0;num != 1; count++){
+		if(num % 2)
+			num = (3 * num + 1) / 2;
 		else
-			n_num /= 2;	
+			num /= 2;	
 	}
-	std::cout << i;
+	std::cout << count;
     return 0;
 }
